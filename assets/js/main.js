aa3b336b6c017ff774c1a1f25b7054b7088ad163
AdrianaHY <adriana.herya@gmail.com>
2017-03-29 12:18:37 -0600

    cuarta versión con input para agregar pendiente y un intento de objeto

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,16 +2,26 @@ var nuevoBoton=document.createElement("button");
 var nombreLista=document.getElementById("nombreLista").value;
 var listasCreadas=document.getElementById("listasCreadas");
 var nuevaLista=document.createElement("ul");
+var pendienteNuevo=document.getElementsByTagName("input").value;
+var inputPendiente = pendienteNuevo;
+
+console.log(pendienteNuevo);
 
 function mostrarLista(){
   var nombreLista=document.getElementById("nombreLista").value;
   var listasCreadas=document.getElementById("listasCreadas");
   var nuevaLista=document.createElement("ul");
+  var pendienteUno=document.createElement("li");
+  //pendienteUno=inputPendiente;
 
   nuevaLista.value=nombreLista;
-  nuevaLista.appendChild(nuevoBoton);
+  nuevaLista.id="nueva";
+  nuevaLista.appendChild(pendienteUno);
   listasCreadas.appendChild(nuevaLista);
+
   listasCreadas.innerText=nombreLista;
+
+  agregarInput();
   botonLista();
 
   console.log(nombreLista);
@@ -20,13 +30,62 @@ function mostrarLista(){
 
 function botonLista(){
   var boton = document.createElement("button");
-  var nombreBoton = document.createTextNode("Agregar pendiente");
+  var nombreBoton = document.createTextNode("Crear pendiente");
   boton.appendChild(nombreBoton);
   document.body.appendChild(boton);
+  //agregarPendiente();
   //nuevaLista.appendChild(boton);
 
 }
-// function agregar
+//Me quedé atorada para obtener los valores del input
+//Pensé que con un objeto lista sería bueno
+// function Lista(pendiente,nombre){
+//   this.nombre = document.getElementById("nombreLista").value;
+//   this.agregarPendiente = function(){
+//                           var inputPendiente = document.createElement("input");
+//                           inputPendiente.value="";
+//                           document.body.appendChild(inputPendiente);
+//                           console.log(inputPendiente)
+//                           }
+//   this.crearPendiente = function(){
+//                         var boton = document.createElement("button");
+//                         var nombreBoton = document.createTextNode("Crear pendiente");
+//                         boton.appendChild(nombreBoton);
+//                         document.body.appendChild(boton);
+//                         agregarPendiente();
+//                         }
+//   this.pendiente = pendiente;
+//
+// }
+//
+//
+// var listas=[];
+// function crearLista(){
+//
+//     var unaLista = new Lista(nombre.value,
+//                               agregarPendiente(),
+//                               crearPendiente());
+//     listas.push(unaLista);
+//     // mostrarLista();
+//
+//
+//   }
+//
+//
+//   function mostrarLista(){
+//
+//     var listaFinal=document.createElement("ul");
+//     var listasCreadas=document.createTextNode(nombreLista.value);
+//     listaFinal.appendChild(listasCreadas);
+//     var listasMuchas=document.getElementById("listasCreadas").appendChild(listaFinal);
+// }
+function agregarInput(){
+  var inputPendiente = document.createElement("input");
+  // var nombreInput = document.createElement("Tarea pendiente");
+  // inputPendiente.appendChild(nombreInput);
+  inputPendiente.value="";
+  //inputPendiente.id=Date.Now();
+  document.body.appendChild(inputPendiente);
+  console.log(inputPendiente);
 
-// var nuevoItem=document.createElement("li");
-// nuevaLista.appendChild(nuevoItem);
+}
