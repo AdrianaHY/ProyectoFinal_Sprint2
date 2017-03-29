c78d1e874bd5fde9a3b1f3dbed7129e9ef2ae2c0
AdrianaHY <adriana.herya@gmail.com>
2017-03-29 10:16:29 -0600

    Tercera versión con botón de agregar pendiente

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,32 @@
+var nuevoBoton=document.createElement("button");
+var nombreLista=document.getElementById("nombreLista").value;
+var listasCreadas=document.getElementById("listasCreadas");
+var nuevaLista=document.createElement("ul");
+
 function mostrarLista(){
   var nombreLista=document.getElementById("nombreLista").value;
   var listasCreadas=document.getElementById("listasCreadas");
   var nuevaLista=document.createElement("ul");
+
   nuevaLista.value=nombreLista;
+  nuevaLista.appendChild(nuevoBoton);
   listasCreadas.appendChild(nuevaLista);
   listasCreadas.innerText=nombreLista;
+  botonLista();
 
   console.log(nombreLista);
 
 }
-function agregar
+
+function botonLista(){
+  var boton = document.createElement("button");
+  var nombreBoton = document.createTextNode("Agregar pendiente");
+  boton.appendChild(nombreBoton);
+  document.body.appendChild(boton);
+  //nuevaLista.appendChild(boton);
+
+}
+// function agregar
 
 // var nuevoItem=document.createElement("li");
 // nuevaLista.appendChild(nuevoItem);
